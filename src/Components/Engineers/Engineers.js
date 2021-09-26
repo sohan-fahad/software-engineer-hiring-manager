b4e7916ef417eb2717cd616daa54674dcba8bd2b
sohan-fahad <sohanfahad@gmail.com>
2021-09-26 14:30:59 +0600

    error fixed

--- src/Components/Engineers/Engineers.js
+++ src/Components/Engineers/Engineers.js
@@ -4,16 +4,22 @@ import './Engineers.css';
 import Cart from '../Cart/Cart'
 
 const Engineers = () => {
+    // This state for displaying engineers info
     const [engineers, setEngineers] = useState([])
+
+    // This state for showing the selected for hire in cart
     const [totalHired, setTotalHire] = useState([])
+
+    // This state for showing the selected hiring employee name
     const [displayHiredName, seDisplayHiredName] = useState([])
+
     useEffect(() => {
         fetch('./fakeDb.JSON')
             .then(res => res.json())
             .then(data => setEngineers(data))
     }, [])
 
-    // 
+    // this even hanler for showing selected data in cart
     let handleHireCart = engineer => {
         const newEngineersHireInfo = [...totalHired, engineer]
         setTotalHire(newEngineersHireInfo)
